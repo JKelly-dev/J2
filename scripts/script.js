35c02f9a9b6dad500699a1e0a50e01aa9ec6294e
Jack Kelly <kcjackkelly@gmail.com>
2019-11-19 20:25:03 -0600

    Added different arrow, and box shadows on images.

--- scripts/script.js
+++ scripts/script.js
@@ -4,19 +4,19 @@ const loadCircle = document.querySelector('div.circle');
 const j2logo = document.querySelector('#loading img');
 const circleMenuLeft = document.getElementById('circleLeft');
 const circleMenuRight = document.getElementById('circleRight');
-const circleMenuBottom = document.getElementById('circleBottom');
+const circleMenuBottomBackground = document.getElementById('circleBottomBackground');
 const homeSectionText = document.querySelector('#homeSection p');
 const workSectionSlideText = document.querySelector('#workSection #slide1 p');
 var circleMenuLeftRotation = 0;
 var circleMenuRightRotation = 0;
-
+var circleMenuBottomBackgroundRotation = 0;
 function contentChange () {
     if (window.innerWidth > 600) {
-        homeSectionText.textContent = "Scroll down to get started."
-        workSectionSlideText.textContent = "Click an arrow to view our work."
+        homeSectionText.textContent = "Scroll down to get started"
+        workSectionSlideText.textContent = "Click an arrow to view our work"
     } else {
-        homeSectionText.textContent = "Swipe up to get started."
-        workSectionSlideText.textContent = "Swipe left to view our work."
+        homeSectionText.textContent = "Swipe up to get started"
+        workSectionSlideText.textContent = "Swipe left to view our work"
     }
 }
 
@@ -54,13 +54,23 @@ var myFullpage = new fullpage('#fullpage', {
             circleMenuLeft.style.transform = `translateY(-50%) rotate(${circleMenuLeftRotation}deg)`;
             circleMenuRightRotation-=90;
             circleMenuRight.style.transform = `translateY(-50%) rotate(${circleMenuRightRotation}deg)`;
+            circleMenuBottomBackgroundRotation-=90;
+            circleMenuBottomBackground.style.transform = `rotate(${circleMenuBottomBackgroundRotation}deg)`;
             if (origin.anchor == "work") {
-                circleMenuBottom.style.opacity = "0";
+                circleMenuBottomBackground.style.opacity = "0";
             } else if (destination.anchor == "work") {
                 setTimeout(() => {
-                    circleMenuBottom.style.opacity = "1";
+                    circleMenuBottomBackground.style.opacity = "1";
                 }, 300)
             } 
+        },
+        onSlideLeave: function(section, origin, destination, direction){
+            circleMenuLeftRotation+=90;
+            circleMenuLeft.style.transform = `translateY(-50%) rotate(${circleMenuLeftRotation}deg)`;
+            circleMenuRightRotation-=90;
+            circleMenuRight.style.transform = `translateY(-50%) rotate(${circleMenuRightRotation}deg)`;
+            circleMenuBottomBackgroundRotation-=90;
+            circleMenuBottomBackground.style.transform = `rotate(${circleMenuBottomBackgroundRotation}deg)`;
         },
         loopBottom: true,
         loopTop: true
