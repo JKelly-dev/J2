140a6cf4c120ebe4316bcd79f7e2e3757eed162c
MyManVan <kcjackkelly@gmail.com>
2019-12-03 14:25:06 -0600

    Added loading code into html

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,10 @@ const loadCircle = document.querySelector('div.circle');
 const j2logo = document.querySelector('#loading img');
 const circleMenuLeft = document.getElementById('circleLeft');
 const circleMenuRight = document.getElementById('circleRight');
-const circleMenuBottom = document.getElementById('circleBottom');
 const homeSectionText = document.querySelector('#homeSection p');
 const workSectionSlideText = document.querySelector('#workSection #slide1 p');
 var circleMenuLeftRotation = 0;
 var circleMenuRightRotation = 0;
-var circleMenuBottomRotation = 0;
 function contentChange () {
     if (window.innerWidth > 600) {
         homeSectionText.textContent = "Scroll down to get started";
@@ -20,25 +18,7 @@ function contentChange () {
     }
 }
 
-window.addEventListener('load', () => {
-    loadCircle.addEventListener('animationend', () => {
-        if (loadCircle.classList.contains('spin')) {
-            loadCircle.classList.remove('spin');
-            loadCircle.classList.add('fullwidth');
-            j2logo.classList.add('j2hide');
-            j2logo.style.opacity = "0";
-            setTimeout(() => {
-                loadingSection.style.backgroundColor = "transparent";
-                j2logo.style.opacity = "0";
-                j2logo.style.visibility = "hidden";
-            }, 1000)
-        } else if (loadCircle.classList.contains('fullwidth') && j2logo.classList.contains('j2hide')) {
-            loadCircle.classList.remove('fullwidth');
-            loadCircle.style.visibility = "hidden";
-            loadCircle.classList.remove('j2hide');
-            loadingSection.style.height = "0px"; 
-        }
-    });
+document.addEventListener('load', function() {
     contentChange();
 });
 
@@ -46,7 +26,6 @@ var myFullpage = new fullpage('#fullpage', {
         licenseKey: '32FDC319-24F94392-ABCB0861-ECB0F5E9',
         anchors: ['home', 'about', 'work', 'talk'],
         navigation: true,
-        slidesNavigation: true,
         navigationPosition: 'left',
         scrollingSpeed: 400,
         onLeave: function(origin, destination, direction){
@@ -54,23 +33,12 @@ var myFullpage = new fullpage('#fullpage', {
             circleMenuLeft.style.transform = `translateY(-50%) rotate(${circleMenuLeftRotation}deg)`;
             circleMenuRightRotation-=90;
             circleMenuRight.style.transform = `translateY(-50%) rotate(${circleMenuRightRotation}deg)`;
-            circleMenuBottomRotation-=90;
-            circleMenuBottom.style.transform = `translateX(-50%) rotate(${circleMenuBottomRotation}deg)`;
-            if (origin.anchor == "work") {
-                circleMenuBottom.style.opacity = "0";
-            } else if (destination.anchor == "work") {
-                setTimeout(() => {
-                    circleMenuBottom.style.opacity = "1";
-                }, 300)
-            } 
         },
         onSlideLeave: function(section, origin, destination, direction){
             circleMenuLeftRotation+=90;
             circleMenuLeft.style.transform = `translateY(-50%) rotate(${circleMenuLeftRotation}deg)`;
             circleMenuRightRotation-=90;
             circleMenuRight.style.transform = `translateY(-50%) rotate(${circleMenuRightRotation}deg)`;
-            circleMenuBottomRotation-=90;
-            circleMenuBottom.style.transform = `translateX(-50%) rotate(${circleMenuBottomRotation}deg)`;
         },
         loopBottom: true,
         loopTop: true
