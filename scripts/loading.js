5b4c19353cbfe5b5d816d39d06b97541b449d460
MyManVan <kcjackkelly@gmail.com>
2020-01-29 14:51:48 -0600

    Changed loader and form
    Moved inline style and javascript to respective files. Added phone and contact input in form. Made form responsive for mobile and desktop. Changed menu dot size.

---
+++ scripts/loading.js
@@ -0,0 +1,24 @@
+const loadingSection = document.getElementById('loading');
+const loadStop = document.getElementById('loadstop');
+const loadCircle = document.querySelector('div.circle');
+const j2logo = document.querySelector('#loading img');
+window.addEventListener('load', () => {
+    loadCircle.addEventListener('animationend', () => {
+        if (loadCircle.classList.contains('spin')) {
+            loadCircle.classList.remove('spin');
+            loadCircle.classList.add('fullwidth');
+            j2logo.classList.add('j2hide');
+            j2logo.style.opacity = "0";
+            setTimeout(() => {
+                loadingSection.style.backgroundColor = "transparent";
+                j2logo.style.opacity = "0";
+                j2logo.style.visibility = "hidden";
+            }, 1000)
+        } else if (loadCircle.classList.contains('fullwidth') && j2logo.classList.contains('j2hide')) {
+            loadCircle.classList.remove('fullwidth');
+            loadCircle.style.visibility = "hidden";
+            loadCircle.classList.remove('j2hide');
+            loadingSection.style.height = "0px"; 
+        }
+    });
+});
