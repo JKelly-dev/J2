d0089c7eca46639d0b4ba12740379e884ac49240
MyManVan <kcjackkelly@gmail.com>
2020-02-19 12:36:13 -0600

    Merged loading code.
    Updated 404 page.

--- scripts/script.js
+++ scripts/script.js
@@ -92,4 +92,31 @@ const floatlabels = new FloatLabels( '#talkForm', {
     style: 2
 });
     
+window.addEventListener('load', () => {
+    const loadingSection = document.getElementById('loading');
+    const loadCircle = document.querySelector('div.circle');
+    const j2logo = document.querySelector('#loading img');
+    setTimeout(() => {
+        loadCircle.addEventListener('animationend', () => {
+            if (loadCircle.classList.contains('spin')) {
+                loadCircle.classList.remove('spin');
+                loadCircle.classList.add('fullwidth');
+                j2logo.classList.add('j2hide');
+                j2logo.style.opacity = "0";
+                setTimeout(() => {
+                    loadingSection.style.backgroundColor = "transparent";
+                    j2logo.style.opacity = "0";
+                    j2logo.style.visibility = "hidden";
+                }, 1000)
+            } else if (loadCircle.classList.contains('fullwidth') && j2logo.classList.contains('j2hide')) {
+                loadCircle.classList.remove('fullwidth');
+                loadCircle.style.visibility = "hidden";
+                loadCircle.classList.remove('j2hide');
+                loadingSection.style.height = "0px"; 
+            }
+        });
+    }, 200);
+});
+
 pageMethods.initEventListener();
+document.querySelector(domStrings.homePageVideo).play();
